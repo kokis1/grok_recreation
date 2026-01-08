afd2492616a86f18d869c7373f16e3276616854d
kokis1 <reuben.stannah@icloud.com>
2026-01-08 13:24:48 +0000

    created the model and training loop

--- code/dataset_generation.py
+++ code/dataset_generation.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 
-filename = "../datasets/dataset.csv"
+filename = "../datasets/dataset_addition.csv"
 
 two_digit_numbers = np.arange(100)
 
@@ -9,9 +9,9 @@ two_digit_numbers = np.arange(100)
 dataset = np.zeros((2, 3))
 for num_1 in two_digit_numbers:
    for num_2 in two_digit_numbers:
-      row = np.array((num_1, num_2, num_1+num_2))
+      row = np.array((num_1, num_2, num_1+num_2), dtype=np.int16)
       dataset = np.vstack((dataset, row))
 
-dataset = pd.DataFrame(dataset)
+dataset = pd.DataFrame(dataset, dtype=np.int64)
 
 dataset.to_csv(filename, index=False, index_label=False, header=False)
---
+++ code/model.py
@@ -0,0 +1,139 @@
+import pandas as pd
+import numpy as np
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import TensorDataset, DataLoader
+import torch
+
+class GrokNN(nn.Module):
+   def __init__(self, input_size, hidden_size, output_size):
+      super().__init__()
+
+      self.dense_1 = nn.Linear(input_size, hidden_size)  # First layer
+      self.dense_2 = nn.Linear(hidden_size, hidden_size)  # Second hidden layer
+      self.dense_3 = nn.Linear(hidden_size, output_size)  # Output layer
+      self.relu = nn.ReLU()  # Activation function
+   
+   def forward(self, x):
+      x = self.dense_1(x)  # Pass through first layer
+      x = self.relu(x)  # Apply activation
+      x = self.dense_2(x)  # Second layer
+      x = self.relu(x)  # Activation
+      x = self.dense_3(x)  # Output layer
+      return x
+
+
+
+def split_data(data: np.ndarray, ratio: float) -> tuple[np.ndarray: 4]:
+   # permutes the data
+   data = np.random.permutation(data)
+   length = int(data.shape[0] * ratio)
+   training = data[:length]
+   testing = data[length:]
+
+   dtrain = training[:, :2]
+   ltrain = training[:, 2]
+
+   dtest = testing[:, :2]
+   ltest = testing[:, 2]
+
+   return dtrain, ltrain, dtest, ltest
+
+def training(model, optimizer, criterion, num_epochs, train_loader, test_loader):
+   
+   metrics = pd.DataFrame({"Epoch":[0], 
+                               "train loss":[0], 
+                               "train correct":[0], 
+                               "test loss":[0], 
+                               "test correct":[0]})
+   
+   train_losses = []
+   train_corrects = []
+   
+   test_losses = []
+   test_corrects = []
+   
+   
+   for epoch in range(num_epochs):
+      model.train()
+      train_loss = 0
+      train_correct = 0
+
+      for inputs, labels in train_loader:
+         optimizer.zero_grad()
+         outputs = model(inputs)
+         loss = criterion(outputs, labels)
+         loss.backward()
+         optimizer.step()
+
+         train_loss += loss.item()
+         preds = torch.argmax(outputs, dim=1)
+         train_correct += (preds == labels).sum().item()
+      
+      model.eval()
+      test_loss = 0
+      test_correct = 0
+      with torch.no_grad():
+        for inputs, labels in test_loader:
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            
+            test_loss += loss.item()
+            preds = torch.argmax(outputs, dim=1)
+            test_correct += (preds == labels).sum().item()
+      
+      train_corrects.append(train_correct)
+      test_corrects.append(test_correct)
+      
+      train_losses.append(train_losses)
+      test_losses.append(test_losses)
+      
+      if epoch+1 % 50 == 0:
+         print(epoch, train_loss, test_loss)
+   
+   metrics = pd.DataFrame({"Epoch":np.arange(num_epochs), 
+                              "train loss":train_losses, 
+                              "train correct":train_corrects, 
+                              "test loss":test_losses, 
+                              "test correct":test_corrects})
+   return metrics
+
+def main():
+   data_filepath = "../datasets/dataset_addition.csv"
+   results_filepath = "../results/metrics_addition.csv"
+   data = pd.read_csv(data_filepath).to_numpy()
+   training_ratio = 0.3
+   
+   dtrain, ltrain, dtest, ltest = split_data(data, training_ratio)
+
+   input_size = 2
+   output_size = 199
+   hidden_size = 128
+   decay_rate = 0.05
+   num_epochs = 1000
+
+
+   train_dataset = TensorDataset(
+    torch.FloatTensor(dtrain),
+    torch.LongTensor(ltrain)  # LongTensor for class indices
+)
+   data_loader = DataLoader(train_dataset, batch_size=512, shuffle=True)
+
+   # Same for validation
+   val_dataset = TensorDataset(
+      torch.FloatTensor(dtest),
+      torch.LongTensor(ltest)
+   )
+   test_loader = DataLoader(val_dataset, batch_size=512, shuffle=False)
+
+   model = GrokNN(input_size, hidden_size, output_size)
+
+   loss = nn.CrossEntropyLoss()
+   optimizer = optim.Adam(model.parameters(), weight_decay=decay_rate)
+
+   metrics = training(model, optimizer, loss, num_epochs, data_loader, test_loader)
+   
+   metrics.to_csv(results_filepath, index=False, index_label=False)
+
+if __name__ == "__main__":
+   main()
